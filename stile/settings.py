172814ab182bccdd99c8a0f7da1cda5b1b2f00d8
sophieboyle1 <sophieboylemiami@gmail.com>
2021-03-07 12:37:52 +0000

    changed secret key

--- stile/settings.py
+++ stile/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a$8szg13iadbj^$(avz0^*z3t8bu)g*hr-*n%_dcm#(01qx83n'
+SECRET_KEY = os.environ.get('SECRET_KEY', '/')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
