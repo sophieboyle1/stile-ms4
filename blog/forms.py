4b4730713247c8119ad13024a1e87be648a5a8cf
sophieboyle1 <sophieboylemiami@gmail.com>
2021-03-15 10:02:13 +0000

    add forms.py to blog

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'article']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['article'].label = "Comment"
