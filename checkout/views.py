ad51ace4381f0b68e37c3e2df0f3c446aed6ed5f
sophieboyle1 <sophieboylemiami@gmail.com>
2021-03-15 13:18:34 +0000

    add stripe keys to checkout;made js and extra css templates

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51INwjeEp80xM5d3OehEBRk3kYPpPTJDMRV5Q5fHTqOwfVrSEYx0MguRK6hdOFupqb2pRJ2gTYmIgB664IzEXsxkl0055rjE8oh',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
