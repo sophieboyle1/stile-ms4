90acae9535cc3d3d8ee93a11584860559fb9838f
sophieboyle1 <sophieboylemiami@gmail.com>
2021-03-19 09:01:17 +0000

    secure product admin actions

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
 
@@ -18,6 +19,7 @@ def profile(request):
     return render(request, template, context)
 
 
+@login_required
 def order_history(request):
     """ Display the user's order history. """
 
@@ -33,6 +35,7 @@ def order_history(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_profile(request):
     """ Display the user's edit profile details form. """
 
