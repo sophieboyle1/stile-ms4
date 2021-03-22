6a417808907a552035358b197fa8007ce9d7e4ca
sophieboyle1 <sophieboylemiami@gmail.com>
2021-03-22 10:26:46 +0000

    add review template; update view

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Review
 
 
 def reviews(request):
-    return render(request, 'reviews/includes/reviews.html')
+    reviews = Review.objects.all()
+    template = 'reviews/includes/reviews.html'
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, template, context)
