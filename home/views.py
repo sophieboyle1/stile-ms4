bb236d8a555f4461e8938f77f8191d68257e8b8c
sophieboyle1 <sophieboylemiami@gmail.com>
2021-03-23 15:30:07 +0000

    improve hp new arrivals products gallery

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from products.models import Product
 
 
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    featured_product_1 = get_object_or_404(Product, pk=43)
+    featured_product_2 = get_object_or_404(Product, pk=44)
+    featured_product_3 = get_object_or_404(Product, pk=45)
+
+    template = 'home/index.html'
+    context = {
+        'featured_product_1': featured_product_1,
+        'featured_product_2': featured_product_2,
+        'featured_product_3': featured_product_3,
+    }
+
+    return render(request, template, context)
